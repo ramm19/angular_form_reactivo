ddecd032b82dd4eb334e44bb015831f74251ad89
Alejandro Menjivar <alejandro.menjivar88@gmail.com>
2025-12-18 00:28:42 -0600

    estructara app

---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-page',
+  imports: [],
+  templateUrl: './dynamic-page.component.html',
+  styles: ``,
+})
+export class DynamicPageComponent {
+
+}
